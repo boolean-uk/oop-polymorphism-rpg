0791fee6aa7e4270592595e49f522f379de82f57
Tim Zoltie <t.zoltie@gmail.com>
2024-06-11 16:29:17 +0100

    hitsObsorbed method

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,3 +1,4 @@
+import exp from "constants"
 import Character, {Player, Enemy1 } from "../src/index.js"
 
 describe('Character', () => {
@@ -11,5 +12,19 @@ describe('Character', () => {
         let attacker = enemy
         character.takeDamage(attacker)
         expect(attacker.currentHitPoints).toBe(25)
+        expect(character.currentHitPoints).toBe(30)
+    })
+})
+describe('Player and Enemies', () => {
+    let player
+    let enemy
+    beforeEach(() => {
+        player = new Player()
+        enemy = new Enemy1()
+    })
+    it('should obsorb hits from an attacker which calls the takeDamge method', () => {
+        player.hitsObsorbed(enemy)
+        expect(player.currentHitPoints).toBe(25)
+        expect(enemy.currentHitPoints).toBe(30)
     })
 })

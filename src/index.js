1efcf01a0f6b572b7a4dc8135ffaa4402f47b447
Tim Zoltie <t.zoltie@gmail.com>
2024-06-11 11:38:22 +0100

    player and enemies class created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 class Character  {
-    constructor(name) {
-        this.name = name
+    constructor() {
+        this.name = ''
         this.maxHitPoints = 30
         this.currentHitPoints = 30
         this.damage = 5
@@ -10,3 +10,30 @@ class Character  {
         attacker.currentHitPoints -= this.damage
     }
 }
+
+class Player extends Character {
+    constructor() {
+        super().name = 'Horne'
+    }
+}
+
+class Enemy1 extends Character {
+    constructor() {
+        super().name = 'Villmata'
+    }
+}
+
+class Enemy2 extends Character {
+    constructor() {
+        super.name = 'Bearl'
+    }
+}
+
+class Enemy2 extends Character {
+    constructor() {
+        super().name = 'Clawes'
+    }
+}
+
+const playerInstance = new Player()
+console.log(playerInstance.name)

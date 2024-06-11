48e02410e149163669e32a1c310511cd241a0924
Alistair Henshaw <alistairhenshaw@gmail.com>
2024-06-11 21:52:11 +0100

    core hopefully

--- src/index.js
+++ src/index.js
@@ -16,12 +16,56 @@ class Player extends Character {
     }
 }
 
+class Wolf extends Character {
+    constructor(name, hitPoints, damage) {
+        super(name, hitPoints, damage)
+    }
+}
+
+class Goblin extends Character {
+    constructor(name, hitPoints, damage) {
+        super(name, hitPoints, damage)
+    }
+}
+
+class Undead extends Character {
+    constructor(name, hitPoints, damage) {
+        super(name, hitPoints, damage)
+    }
+}
+
+class Brawler extends Character {
+    constructor(name, hitPoints, damage) {
+        super(name, hitPoints, damage)
+    }
+}
+
+class BattleScene {
+    fight(characterOne, characterTwo) {
+        while (characterOne.currentHitPoints > 0 && characterTwo.currentHitPoints > 0) {
+            characterTwo.takeDamage(characterOne)
+            console.log(`${characterTwo.name} took ${characterOne.damage} damage`)
+            if (characterTwo.currentHitPoints > 0) {
+                characterOne.takeDamage(characterTwo)
+                console.log(`${characterOne.name} took ${characterTwo.damage} damage`)
+            }
+            if (characterTwo.currentHitPoints <= 0) {
+                return characterOne.name
+            }
+            if (characterOne.currentHitPoints <= 0) {
+                return characterTwo.name
+            }
+        }
+    }
+}
 
 
-const me = new Player ("Me", 20, 5)
-console.log(me)
-console.log(me.damage)
-me.takeDamage({damage: 13})
-console.log(me)
+
+// const me = new Player ("Me", 20, 5)
+// console.log(me)
+// console.log(me.damage)
+// me.takeDamage({damage: 13})
+// console.log(me)
 
 export default Character
+export { Player, Undead, BattleScene }
